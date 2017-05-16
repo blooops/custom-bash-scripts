067fe02e9237ea86e25eb93719fec9d946d960b9
roy <rohit31roy97coder@gmail.com>
2017-05-16 12:36:50 +0530

    minor changes

--- listscripts.sh
+++ listscripts.sh
@@ -1,3 +1,4 @@
 # Lists the complete set of custom scripts available and their short descriptions.
 
 echo "makedoc: [filename ... => creates the respective files"
+echo "listscripts: => lists the available custom scripts and their descriptions"
