598940a0602607fbc2f6468c59bc563dea58d2ba
roy <rohit31roy97coder@gmail.com>
2017-05-22 16:05:09 +0530

    added tmux-session.sh for managing tmux sessions

--- listscripts.sh
+++ listscripts.sh
@@ -2,3 +2,4 @@
 
 echo "makedoc: [filename ... => creates the respective files"
 echo "listscripts: => lists the available custom scripts and their descriptions"
+echo "tmux-session [save|restore] => restore or save your tmux session for boot"
